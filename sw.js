5184843cf5e584ed832e6a9a1962e76d31891514
n.mihajlovic <n.mihajlovic@frontdeskanywhere.com>
2021-09-15 21:32:13 +0200

    uncomented self.__WB_MANIFEST

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@ import { CacheFirst, NetworkFirst, NetworkOnly } from 'workbox-strategies';
 
 workbox.setConfig({ debug: true });
 // workbox.skipWaiting();
-// workbox.precaching.precacheAndRoute(self.__WB_MANIFEST)
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST)
 
 precacheAndRoute([
   // { url: '/', revision: 'dasdasczxczxc2323' }, - Ne moze da bude ovde jer onda dodavanje novog posta ne bude procitano. 
